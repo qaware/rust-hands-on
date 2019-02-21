afaa7449cf10179f379edb1cc47323be9ecb77b4
Fabian Huch <fabian.huch@qaware.de>
2019-02-21 19:03:08 +0100

    Finished echo server and client

--- projects/2-client-server/client/src/main.rs
+++ projects/2-client-server/client/src/main.rs
@@ -1,33 +1,82 @@
+extern crate clap;
 extern crate log;
 extern crate simple_logger;
 
-use log::info;
 use std::io;
-use std::io::Read;
-use std::io::Write;
-use std::net::Shutdown;
-use std::net::TcpStream;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{Shutdown, TcpStream, ToSocketAddrs};
 
-const SERVER_ADDRESS: &str = "127.0.0.1:6666";
+use clap::{App, Arg};
+use log::info;
+use std::io::ErrorKind::{TimedOut, WouldBlock};
+use std::time::Duration;
+
+const READ_TIMEOUT_MS: u64 = 100;
+const MESSAGE: [&str; 7] = [
+    "HELO client.example.com\n",
+    "MAIL FROM:<mail@samlogic.com>\n",
+    "RCPT TO:<john@mail.com>\n",
+    "DATA\n",
+    "<The message data (body text, subject, e-mail header, attachments etc) is sent>\n",
+    ".\n",
+    "QUIT",
+];
 
 fn main() -> io::Result<()> {
+    let matches = App::new("SMTP mock client")
+        .version("1.0")
+        .author("Fabian Huch <fabian.huch@qaware.de>")
+        .arg(
+            Arg::with_name("ADDRESS")
+                .required(true)
+                .validator(|s| s.to_socket_addrs().and(Ok(())).map_err(|e| e.to_string())),
+        )
+        .get_matches();
+
     // Initialize logger
     simple_logger::init().expect("Could not initialize logger");
 
     // Connect to server
-    let mut stream = TcpStream::connect(SERVER_ADDRESS)?;
+    let mut stream = TcpStream::connect(matches.value_of("ADDRESS").unwrap())?;
+    stream.set_read_timeout(Some(Duration::from_millis(READ_TIMEOUT_MS)))?;
+    stream.set_write_timeout(None)?;
 
-    let message = String::from("Hello world");
+    // Reader to read single lines
+    let mut reader = BufReader::new(stream.try_clone()?);
 
-    // Write message and EOF
-    stream.write_all(message.as_bytes())?;
-    stream.shutdown(Shutdown::Write)?;
+    // Receive initial message, if any:
+    match receive_answer(&mut reader)? {
+        Some(res) => info!("Received message: {}", res),
+        None => (),
+    }
 
-    // Read result
-    let mut buf = String::new();
-    stream.read_to_string(&mut buf)?;
+    for (i, line) in MESSAGE.iter().enumerate() {
+        // Write line
+        stream.write_all(line.as_bytes())?;
 
-    info!("Wrote \"{}\", received \"{}\"", message, buf);
+        // Shutdown write stream if last line was written
+        if i == MESSAGE.len() - 1 {
+            stream.shutdown(Shutdown::Write)?;
+        }
+
+        // Receive answer, if any
+        match receive_answer(&mut reader)? {
+            Some(res) => info!("Wrote {:?}, received {:?}", line, res),
+            None => info!("Wrote {:?}, received nothing", line),
+        }
+    }
 
     Ok(())
+}
+
+fn receive_answer(reader: &mut BufRead) -> io::Result<Option<String>> {
+    let mut buf = String::new();
+    match reader.read_line(&mut buf) {
+        Ok(_) => Ok(Some(buf)),
+        Err(e) => match e.kind() {
+            // Timed out -> no answer
+            WouldBlock | TimedOut => Ok(None),
+            _ => return Err(e),
+        },
+    }
 }
--- projects/2-client-server/echo-server/src/main.rs
+++ projects/2-client-server/echo-server/src/main.rs
@@ -1,43 +1,54 @@
+extern crate clap;
 extern crate log;
 extern crate simple_logger;
 
-use log::{error, info};
-use std::io::{Read, Write};
-use std::net::Shutdown;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::sync::Arc;
-use std::sync::Mutex;
-use std::time::Duration;
+use clap::{App, Arg};
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 use std::{io, thread};
 
-const SERVER_ADDRESS: &str = "127.0.0.1:6666";
+use log::{error, info};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::time::Duration;
 
 fn main() -> io::Result<()> {
     // Initialize logger
     simple_logger::init().expect("could not initialize logger");
 
-    let listener = TcpListener::bind(SERVER_ADDRESS)?;
+    let matches = App::new("Simple Echo Server")
+        .version("1.0")
+        .author("Fabian Huch <fabian.huch@qaware.de>")
+        .arg(
+            Arg::with_name("PORT")
+                .required(true)
+                .validator(|s| s.parse::<u16>().and(Ok(())).map_err(|e| e.to_string())),
+        )
+        .get_matches();
+
+    let port = matches.value_of("PORT").unwrap().parse::<u16>().unwrap();
+    let localhost_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), port);
+    let listener = TcpListener::bind(localhost_addr)?;
 
     let msg_history = Arc::new(Mutex::new(Vec::<String>::new()));
 
     // Wait for incoming connections
     for stream in listener.incoming() {
-        // Cloning the Arc will get a new pointer to the same underlying mutex on the data
-        let msg_history = Arc::clone(&msg_history);
-
-        // Spawn new thread for every connection
-        thread::spawn(move || match stream {
+        match stream {
             Ok(stream) => {
                 info!("Connection accepted.");
 
-                match echo(stream, msg_history) {
+                // Cloning the Arc will get a new pointer to the same underlying mutex on the data
+                let msg_history = Arc::clone(&msg_history);
+
+                // Spawn new thread for every connection
+                thread::spawn(move || match echo(stream, msg_history) {
                     Ok(_) => info!("Connection handled successfully."),
                     Err(e) => error!("Error during connection: {}", e),
-                }
+                });
             }
             Err(e) => error!("Connection request failed. Cause: {}", e),
-        });
+        }
     }
 
     Ok(())
@@ -45,23 +56,32 @@ fn main() -> io::Result<()> {
 
 /// Echo-server functionality
 fn echo(mut stream: TcpStream, history: Arc<Mutex<Vec<String>>>) -> io::Result<()> {
-    let mut buf = String::new();
-    stream.read_to_string(&mut buf)?;
+    let mut reader = BufReader::new(stream.try_clone()?);
 
-    // Some heavy load here!
-    thread::sleep(Duration::from_secs(10));
+    loop {
+        // Read line
+        let mut line = String::new();
+        reader.read_line(&mut line)?;
+        let is_eof = !line.ends_with('\n');
 
-    // Add new message and print the last three out
-    match history.lock() {
-        Ok(mut data) => {
-            data.push(buf.clone());
-            let last_msgs: Vec<&String> = data.iter().take(3).collect();
-            info!("Last three received messages: {:?}", last_msgs)
+        match history.lock() {
+            Ok(mut data) => {
+                // Add new message and print the last three out
+                info!("Received {:?}", line);
+                data.insert(0, line);
+                let last_msgs: Vec<&String> = data.iter().take(3).collect();
+                info!("Last three received messages: {:#?}", last_msgs);
+                // Echo string
+                stream.write_all(last_msgs[0].as_bytes())?;
+                stream.flush()?;
+            }
+            Err(e) => error!("Could not access history. Cause: {}", e),
         }
-        Err(e) => error!("Could not access history. Cause: {}", e),
+
+        if is_eof {
+            break;
+        }
     }
 
-    // Echo string and write EOF
-    stream.write_all(buf.as_bytes())?;
-    stream.shutdown(Shutdown::Both)
+    Ok(())
 }
